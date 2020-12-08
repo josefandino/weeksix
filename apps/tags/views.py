7d3e83e14eb48935a845dbf7a80ec928246575ea
josefandino <josefandino>
2020-12-07 21:41:12 -0500

    adrew

--- apps/tags/views.py
+++ apps/tags/views.py
@@ -9,32 +9,6 @@ from ..tags.serializer import TagSerializer
 from ..tags.models import Tag
 
 
-<<<<<<< HEAD
-class Tagviewsets(viewsets.ModelViewSet):
-    queryset = Tag.objects.all()
-    serializer_class = TagSerializer
-
-    @action(methods=['GET', 'POST', 'DELETE'], detail=True)
-    def publicaciones(self, request, pk=None):
-        tag = self.get_object()
-        if request.method == 'GET':
-            id = Publication.objects.filter(tags=int(tag.id))
-            serialized = PublicationSerializer(id, many=True)
-            return Response(status=status.HTTP_200_OK, data=serialized.data)
-        if request.method == 'POST':
-            publication = PublicationSerializer(data=request.data)
-            if publication.is_valid():
-                publication.save()
-                return Response(status=status.HTTP_201_CREATED)
-        else:
-                return Response(status=status.HTTP_400_BAD_REQUEST, )
-        if request.method == 'DELETE':
-            publicaciones_id = request.data['id']
-            for publicaciones_id in publicaciones_id:
-                publicaciones = Publication.objects.get(id=int(publicaciones_id))
-                publicaciones.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-=======
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Publication.objects.all()
     serializer_class = PublicationSerializer
@@ -60,4 +34,3 @@ class TagViewSet(viewsets.ModelViewSet):
                 tag = Tag.objects.get(id=int(tag_id))
                 publications.tags.remove(tag)
                 return Response(status=status.HTTP_204_NO_CONTENT)
->>>>>>> 13ddd2046f9259766f16ab407bf620a236704a09
